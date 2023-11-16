e795dd1011dde4fc5f07b5f04bbe519c2b18462a
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-11-16 15:41:21 +0900

    Q3

--- main.c
+++ main.c
@@ -3,14 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void) 
+void swap(int *x, int *y)
 {
-	int i = 4; // 300으로 바꾸면 결과가 300, 300, 4로 다르게 나옴 
-		
-	int* pi = &i;
-	char* pc = &i;
-	
-	
-	printf("%i, %i, %i\n", i, *pi, *pc);
-
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
 }
+int main(int argc, char *argv[])
+{
+	int a = 3;
+	int b = 5;
+	
+	swap(&a, &b);
+	printf("a:%i, b:%i\n", a,b);
+	
+	system("PAUSE");
+	return 0;
+}
+	
