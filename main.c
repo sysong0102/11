08bbc203bcb55360007a376cedbde32dc84fe2fc
sysong0102 <sysong0102@sookmyung.ac.kr>
2023-11-16 15:56:06 +0900

    Q4

--- main.c
+++ main.c
@@ -3,21 +3,25 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void swap(int *x, int *y)
-{
-	int temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
 int main(int argc, char *argv[])
 {
-	int a = 3;
-	int b = 5;
+	int i;
+	int grade[5];
+	int average = 0;
 	
-	swap(&a, &b);
-	printf("a:%i, b:%i\n", a,b);
 	
+	for (i=0;i<5;i++)
+	{
+		printf("grade[%i]=",i);
+		scanf("%d", &grade[i]);
+	}
+	
+	for (i=0;i<5;i++)
+	{
+		printf("grade[%i]= %i\n",i, *(grade + i));
+		average = average + *(grade + i);
+	}
+	printf("average : %i\n", average/5);
 	system("PAUSE");
 	return 0;
 }
